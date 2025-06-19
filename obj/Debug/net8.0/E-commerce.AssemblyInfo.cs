5d6872e2ef4cfe63cbcda0e59c30d293703da5e9
Mohamed Ali <mohamedali.codes@gmail.com>
2025-06-19 20:24:19 +0300

    ScreenShots-Folder

--- obj/Debug/net8.0/E-commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a08048bff69547777424738e930a968556a7da")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
